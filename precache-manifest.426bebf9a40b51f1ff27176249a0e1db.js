4a67db1787afe3a80b9243bb1fc4ea594d571533
IdanHershcovich <idanhershcovich@gmail.com>
2020-06-12 01:13:56 -0500

    Updates

---
+++ precache-manifest.426bebf9a40b51f1ff27176249a0e1db.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2816db113f6dfe4873e6cc8c47d458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac773f5bd3a8faa8590",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "947ebc5fe2862cb13459",
+    "url": "/static/css/main.3206d1d5.chunk.css"
+  },
+  {
+    "revision": "1ac773f5bd3a8faa8590",
+    "url": "/static/js/2.eb7bae1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.eb7bae1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947ebc5fe2862cb13459",
+    "url": "/static/js/main.fe042f30.chunk.js"
+  },
+  {
+    "revision": "792327bbee1637a8c7ea",
+    "url": "/static/js/runtime-main.d6e31891.js"
+  },
+  {
+    "revision": "edf6691754e08433e55957fdf21057c6",
+    "url": "/static/media/Clustering.edf66917.png"
+  },
+  {
+    "revision": "254c265957c764d1150c3f5da1261c33",
+    "url": "/static/media/HolyTesters.254c2659.png"
+  },
+  {
+    "revision": "5a191a6948b6414457215c686b4f7d58",
+    "url": "/static/media/Idan_Hershcovich_2020.5a191a69.pdf"
+  },
+  {
+    "revision": "204f743c80f9203507646137fa3639c7",
+    "url": "/static/media/Profile_Pic.204f743c.jpg"
+  },
+  {
+    "revision": "768f290be4cca584540996594d7e5466",
+    "url": "/static/media/drexel-university.768f290b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f7591616b8f7bbb6bc74a304935d119",
+    "url": "/static/media/social-1_logo-github.6f759161.svg"
+  },
+  {
+    "revision": "03d3878e2535e011b4722cdeda35d69d",
+    "url": "/static/media/social-1_logo-instagram.03d3878e.svg"
+  },
+  {
+    "revision": "631ad0fe6f5a0a512f9db2cd2422401e",
+    "url": "/static/media/social-1_logo-linkedin.631ad0fe.svg"
+  },
+  {
+    "revision": "fc0f89534b660daa58e078d8e868290f",
+    "url": "/static/media/social-1_round-github.fc0f8953.svg"
+  },
+  {
+    "revision": "55a728da3b69ef26001de87284c9e565",
+    "url": "/static/media/social-1_round-instagram.55a728da.svg"
+  },
+  {
+    "revision": "859ab827ec10c3537c3a9253929a27de",
+    "url": "/static/media/social-1_round-linkedin.859ab827.svg"
+  },
+  {
+    "revision": "29902cfe948b582c08c84828bd90ba51",
+    "url": "/static/media/social-1_square-github.29902cfe.svg"
+  },
+  {
+    "revision": "878e6497b85606e38609b370f9a3c5e2",
+    "url": "/static/media/social-1_square-instagram.878e6497.svg"
+  },
+  {
+    "revision": "ce43af6dcead602ba85ae3d82677144e",
+    "url": "/static/media/social-1_square-linkedin.ce43af6d.svg"
+  },
+  {
+    "revision": "3cf83dc5fa2e584ff62f688aaefe89c5",
+    "url": "/static/media/stop.3cf83dc5.png"
+  },
+  {
+    "revision": "7dee8e223a0e003324104407b3a43ea8",
+    "url": "/static/media/stratis.7dee8e22.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dansdevland.com/precache-manifest.21539099200a061ea03958e82aa6ae53.js"
+  "/precache-manifest.426bebf9a40b51f1ff27176249a0e1db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dansdevland.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
